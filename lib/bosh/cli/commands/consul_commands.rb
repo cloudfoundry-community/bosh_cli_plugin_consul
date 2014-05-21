dacf6cbdab4c4d52e9797a543574cd583d980563
Dr Nic Williams <drnicwilliams@gmail.com>
2014-05-21 13:36:26 +1000

    can find consul cluster IPs via deploymentname

--- lib/bosh/cli/commands/consul_commands.rb
+++ lib/bosh/cli/commands/consul_commands.rb
@@ -1,9 +1,17 @@
 module Bosh::Cli::Command
   class ConsulCommands < Base
+    include Bosh::Cli::Validation
+
+    DEFAULT_CONFIG_PATH = File.expand_path('~/.bosh_consul_config')
+
+    def initialize(runner)
+      super(runner)
+      # options[:config] ||= DEFAULT_CONFIG_PATH # hijack Cli::Config
+    end
 
     usage "consul"
     desc  "show consul commands"
-    def cf_help
+    def consul_help
       say("bosh consul commands:")
       nl
       cmds = Bosh::Cli::Config.commands.values.find_all {|c|
@@ -15,7 +23,18 @@ module Bosh::Cli::Command
 
     usage "target consul"
     desc  "target a consul cluster"
-    def target(name_or_hosts)
+    def target_consul(deployment_name)
+      require "bosh/consul"
+      say "Fetching consul cluster info for deployment '#{deployment_name}'..."
+      cluster = Bosh::Consul::Models::Cluster.new(director)
+      cluster.load_from_deployment_name(deployment_name)
+      step("Current consul cluster '#{deployment_name}'", "Cannot find consul cluster '#{deployment_name}'", :fatal) do
+        cluster.valid?
+      end
+      p cluster.consul_ips
+      
+    rescue Bosh::Cli::ValidationHalted
+      err errors.first
     end
 
     usage "consul status"
--- lib/bosh/consul/models/cluster.rb
+++ lib/bosh/consul/models/cluster.rb
@@ -1,5 +1,19 @@
 class Bosh::Consul::Models::Cluster
+  attr_reader :director_client
+  attr_reader :consul_ips
+
+  def initialize(director_client)
+    @director_client = director_client
+  end
+
+  def load_from_deployment_name(deployment_name)
+    @consul_ips = nil
+    if vms = director_client.fetch_vm_state(deployment_name)
+      @consul_ips = vms.map {|vm| vm["ips"].first }
+    end
+  end
+
   def valid?
-    false
+    !consul_ips.empty?
   end
 end
